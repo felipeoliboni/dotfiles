dfc7b8571bc24e5d5d2e4d65a3303d611dcbb249
Felipe Oliboni <foliboni@gmail.com>
2026-05-25 14:27:35 -0300

    'st'

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# OpenClaw Completion
+test -f "/home/felipe/.openclaw/completions/openclaw.fish"; and source "/home/felipe/.openclaw/completions/openclaw.fish"
